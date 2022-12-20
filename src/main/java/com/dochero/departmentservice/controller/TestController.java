cef5b91b5c4e3bad94dc7fe29120f066df492bfb
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-12-20 21:00:19 +0700

    update document revision feign

--- src/main/java/com/dochero/departmentservice/controller/TestController.java
+++ src/main/java/com/dochero/departmentservice/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.dochero.departmentservice.controller;
 
+import com.dochero.departmentservice.client.DocumentRevisionClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private DocumentRevisionClient documentRevisionClient;
+
     @GetMapping("/test")
     public String checkServer() {
         return "Server is up and runing test cicd";
+    }
+
+    @GetMapping("/check-revision")
+    public String checkDocumentRevisionService() {
+        try {
+            return documentRevisionClient.testService();
+        } catch (Exception e) {
+            return "Error, " + e.getMessage();
+        }
     }
 }
